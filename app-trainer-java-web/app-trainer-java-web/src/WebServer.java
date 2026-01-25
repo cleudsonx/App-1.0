28ee15559130b99ccc4a2b06af3d103ad002628e
Cleudson Cavalcante <cleudson.cavalcante@iesb.edu.br>
2026-01-25 19:30:47 -0300

    Ajuste endpoint RefreshHandler

--- app-trainer-java-web/app-trainer-java-web/src/WebServer.java
+++ app-trainer-java-web/app-trainer-java-web/src/WebServer.java
@@ -1,6 +1,7 @@
 import com.sun.net.httpserver.HttpServer;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
+import auth.RefreshHandler;
 
 import api.*;
 import storage.DataStorage;
