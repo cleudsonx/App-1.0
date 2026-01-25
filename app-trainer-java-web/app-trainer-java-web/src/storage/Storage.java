2ae1694642a422f840dccb842d87180c857bd703
Cleudson Cavalcante <cleudson.cavalcante@iesb.edu.br>
2026-01-25 20:56:36 -0300

    Corrigi o classpath do Dockerfile para garantir que o org.json seja encontrado em qualquer ambiente. Expandi a interface Storage para incluir todos os métodos usados nos handlers (aluno e professor) e unifiquei as exceções para Exception.

--- app-trainer-java-web/app-trainer-java-web/src/storage/Storage.java
+++ app-trainer-java-web/app-trainer-java-web/src/storage/Storage.java
@@ -5,12 +5,22 @@ import java.sql.SQLException;
 
 public interface Storage {
     // Métodos de refresh token
-    boolean isRefreshTokenValido(int userId, String token) throws SQLException;
-    void invalidarRefreshToken(int userId, String token) throws SQLException;
-    void salvarRefreshToken(int userId, String token, long expiraEmEpoch) throws SQLException;
+    boolean isRefreshTokenValido(int userId, String token) throws Exception;
+    void invalidarRefreshToken(int userId, String token) throws Exception;
+    void salvarRefreshToken(int userId, String token, long expiraEmEpoch) throws Exception;
 
-    // Métodos de usuário
-    Aluno getAlunoByEmail(String email) throws SQLException;
-    Aluno addAlunoWithHash(String nome, String email, String senhaHash) throws SQLException;
-    // Adicione outros métodos conforme necessário (ex: getAlunoById, updateAluno, etc)
+    // Métodos de aluno
+    Aluno getAlunoByEmail(String email) throws Exception;
+    Aluno addAlunoWithHash(String nome, String email, String senhaHash) throws Exception;
+    Aluno getAlunoById(int id) throws Exception;
+    java.util.List<Aluno> listAlunos(String objetivo, String nivel) throws Exception;
+    Aluno addAluno(String nome, int idade, String objetivo, String nivel, double pesoKg, double alturaCm, String restricoes, String equipamentos, Integer rpe) throws Exception;
+    Aluno updateAluno(int id, java.util.Map<String, String> updates) throws Exception;
+    boolean deleteAluno(int id) throws Exception;
+
+    // Métodos de professor
+    Professor addProfessor(String nome, String especialidade) throws Exception;
+    Professor getProfessorById(int id) throws Exception;
+    java.util.List<Professor> listProfessores(String especialidade) throws Exception;
+    boolean deleteProfessor(int id) throws Exception;
 }
